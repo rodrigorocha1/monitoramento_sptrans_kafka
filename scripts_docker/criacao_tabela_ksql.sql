2bf40192ac554e1eb27eae0fd0111d6cf31b74e7
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-12-07 19:19:01 -0300

    Correção script

--- scripts_docker/criacao_tabela_ksql.sql
+++ scripts_docker/criacao_tabela_ksql.sql
@@ -101,7 +101,7 @@ SELECT
     t.py AS prev_py,
     t.px AS prev_px,
     t.ta AS prev_ta,
-    s.py AS curr_py,SES
+    s.py AS curr_py,
     s.px AS curr_px,
     s.ta AS curr_ta
 FROM ONIBUS_POSICAO_REKEY s
@@ -135,6 +135,23 @@ SELECT
     curr_py AS CURR_PY,
     curr_px AS CURR_PX,
     curr_ta AS CURR_TA,
+    ROUND(
+        velocidade_media(
+            CAST(prev_py AS DECIMAL(9,6)),
+            CAST(prev_px AS DECIMAL(9,6)),
+            CAST(curr_py AS DECIMAL(9,6)),
+            CAST(curr_px AS DECIMAL(9,6)),
+            CONCAT(prev_ta,'Z'),
+            CONCAT(curr_ta,'Z')
+        ), 2
+    ) AS VELOCIDADE_KMH
+FROM ONIBUS_POSICAO_ANTERIOR_ATUAL
+WHERE curr_ta IS NOT NULL AND PREV_PY is not null ;
+
+
+SELECT
+    S_P,
+    C,
     ROUND(
         velocidade_media(
             CAST(prev_py AS DECIMAL(9,6)),
@@ -197,6 +214,29 @@ SELECT
 FROM ONIBUS_POSICAO_ANTERIOR_ATUAL
 WHERE curr_ta IS NOT NULL AND PREV_PY is not null ;
 
+
+CREATE TABLE VELOCIDADE_MEDIA AS
+SELECT
+    S_P,
+    LATEST_BY_OFFSET(C) AS C,
+    LATEST_BY_OFFSET(CURR_TA) AS CURR_TA,
+    LATEST_BY_OFFSET(
+        ROUND(
+            VELOCIDADE_MEDIA(
+                CAST(PREV_PY AS DECIMAL(9, 6)),
+                CAST(PREV_PX AS DECIMAL(9, 6)),
+                CAST(CURR_PY AS DECIMAL(9, 6)),
+                CAST(CURR_PX AS DECIMAL(9, 6)),
+                CONCAT(PREV_TA, 'Z'),
+                CONCAT(CURR_TA, 'Z')
+            ), 2
+        )
+    ) AS VELOCIDADE_KMH
+FROM ONIBUS_POSICAO_ANTERIOR_ATUAL
+WHERE CURR_TA IS NOT NULL
+  AND PREV_PY IS NOT NULL
+GROUP BY S_P
+EMIT CHANGES;
 
 
 SELECT *
